ccabf43ab3b051a76cfe4fed272d9f9e41c2eead
George Corbeanu <138300929+geogeo532@users.noreply.github.com>
2024-09-09 20:03:03 +0300

    Update api.js

--- src/javascript/api.js
+++ src/javascript/api.js
@@ -1,8 +1,8 @@
 export const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': **********************,
-		'X-RapidAPI-Host' *******************: 
+		'X-RapidAPI-Key': '**********************',
+		'X-RapidAPI-Host' 'wft-geo-db.p.rapidapi.com': 
 	}
 };
 
@@ -10,6 +10,6 @@ export const options_url="https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const weather_api_url="https://api.openweathermap.org/data/2.5";
 
-export const weather_apy_key="95550474037a34b8b34e8a64556c9649";
+export const weather_apy_key="********************************************";
 
-export const pexels_api_key="ihwmZxdxERgpvoQaDSQ0F3cwVcINA2xDXyxytUgqEc0oov7LA9saMaB7"
+export const pexels_api_key="***************************************************"
